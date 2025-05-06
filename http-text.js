ecd9e6386d1cc8a1aa2f732ddb7e812a8018fa18
elena <elenapolishchukdev@gmail.com>
2025-05-06 20:30:42 +0300

    npm i nodemon -D

--- http-text.js
+++ http-text.js
@@ -4,3 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    res.end('<h1>Hello world!</h1>');
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}. Press CTRL+C to finish.`);
+})
