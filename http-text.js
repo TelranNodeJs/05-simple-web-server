718a4257075f04d6b0ee46162ed31c345135b177
elena <elenapolishchukdev@gmail.com>
2025-05-06 21:02:16 +0300

    start app with nodemon

--- http-text.js
+++ http-text.js
@@ -4,12 +4,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+//     res.end('<h1>Hello NODE!</h1>');
+// })
+
 const server = http.createServer((req, res) => {
-
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-
-    res.end('<h1>Hello NODE!</h1>');
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    if(req.url==='/user'){
+        res.write('<h1>Hello User!</h1>');
+    }
+    if(req.url==='/user/group'){
+        res.write('<h1>Hello Group!</h1>');
+    }
+    res.end();
 })
+
 
 server.listen(port, () => {
     console.log(`Listening on port ${port}. Press CTRL+C to finish.`);
