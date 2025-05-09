65dfb4674532353604a8a8cb2635eeee582b82a4
elena <elenapolishchukdev@gmail.com>
2025-05-09 09:36:59 +0300

    end

--- http-text.js
+++ http-text.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
     const url =new URL(req.url, `https://${req.headers.host}`);
+    console.log(req.url);// uers?name=lena
+    console.log(req.headers.host);// localhost:9000
     const name = url.searchParams.get("name");
     res.write(`Hello, ${name}!`)
     res.end();
